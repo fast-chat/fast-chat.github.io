2c8752aba5ef50fcaece01b62a0deeab746aba29
EduardKlimenko <edik.klimenko@gmail.com>
2019-11-23 11:38:16 +0200

    fix

--- index/ThisNotification.js
+++ index/ThisNotification.js
@@ -1,32 +1,34 @@
 ThisNotification {
     constructor () {
-        Notification.requestPermission().then((result) => {
-            console.log('Notification', result);
-        });
+        Notification.requestPermission().then(permissionThen);
  
-        this.sendNotification('Верните Линуса!', {
+        sendNotification('Верните Линуса!', {
             body: 'Тестирование HTML5 Notifications',
             icon: 'images/icons/icon-512x512.png',
-            dir: 'auto'
+            dir: 'auto' 
         });
-    } 
-    public sendNotification(title, options) {
+    }
+    permissionThen (result) {
+        console.log('Notification', result);
+    }
+    clickFunc () {
+        alert('Пользователь кликнул на уведомление');  
+    }
+    requestPermission (permission) {
+        if (permission === "granted") {        
+           var notification = new Notification(title, options);
+        } else { 
+            alert('Вы запретили показывать уведомления');
+        };
+    }
+    sendNotification(title, options) {
         if (!window["Notification"]) {
             alert('Ваш браузер не поддерживает HTML Notifications, его необходимо обновить.');
         } else if (Notification.permission === "granted") {
             var notification = new Notification(title, options);
-            function clickFunc() {
-                alert('Пользователь кликнул на уведомление');  
-            };
             notification.onclick = clickFunc;
         } else if (Notification.permission !== 'denied') {
-            Notification.requestPermission((permission) => {
-                if (permission === "granted") {
-                    var notification = new Notification(title, options);
-                } else {
-                    alert('Вы запретили показывать уведомления');  
-                };
-            });
-        } 
+            Notification.requestPermission(requestPermission);
+        };
     }
 }
