487f6b500511e4f77fd888410c89a395042988f8
EduardKlimenko <edik.klimenko@gmail.com>
2019-11-23 16:06:22 +0200

    fb sw

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+importScripts('lib/firebase.js');
+var config = {
+    apiKey: "AIzaSyAnLD5Hcpa-oBDBbrWulV7jKUpxcPHjeaY",
+    authDomain: "my-project-1-18726.firebaseapp.com",
+    databaseURL: "https://my-project-1-18726.firebaseio.com",
+    projectId: "my-project-1-18726",
+    storageBucket: "my-project-1-18726.appspot.com",
+    messagingSenderId: "266216698283"
+};
+
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+    const title = 'Hello World';
+    const options = {
+        body: payload.data.status
+    };
+    return self.ServiceWorkerRegistration.showNotification(title, options);
+})
--- index.js
+++ index.js
@@ -3,4 +3,17 @@ import index<IndexController>;
 Main {
 	$.css('css/style.css');
 	this.controller = new IndexController();
+
+	var messaging = firebase.messaging();
+	messaging.requestPermission()
+	.then(() => {
+		console.log('Have permission');
+		return messaging.getToken();
+	})
+	.then(token => {
+		console.log(token);
+	})
+	.catch(err => {
+		console.log('Error Ocored.');
+	})
 }
